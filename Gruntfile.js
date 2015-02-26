e8a2da745e8ec42a4d3c401f127e93b7eaca8abe
Ammar Kalim <ammar.kalim@gmail.com>
2015-02-25 23:09:29 -0500

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,97 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		uglify: {
+			options: {
+				style: 'compressed'
+			},
+			build: {
+				src: 'js/main.js',
+				dest: 'js/main.min.js'
+			}
+		},
+
+		autoprefixer: {
+			global: {
+				src: "css/main-unprefixed.css",
+				dest: "css/main.css"
+			}
+		},
+
+
+		shell: {
+			jekyllServe: {
+				command: "jekyll serve"
+			},
+			jekyllBuild: {
+				command: "jekyll build"
+			}
+		},
+
+		sass: {
+			dist: {                            // Target
+				options: {                       // Target options
+					style: 'compressed'
+				},
+				files: {                         // Dictionary of files
+					'css/main-unprefixed.css': 'sass/main.scss',       // 'destination': 'source'
+
+				}
+			}
+		},
+
+		imagemin: {
+			dynamic: {                         // Another target
+				files: [{
+					expand: true,                  // Enable dynamic expansion
+					cwd: 'images',                   // Src matches are relative to this path
+					src: ['**/*.{png,jpg,gif,svg}'],   // Actual patterns to match
+					dest: 'images'                  // Destination path prefix
+				}]
+			}
+		},
+
+		watch: {
+			options: {
+				livereload: true,
+			},
+			site: {
+				files: ["index.html", "_layouts/*.html", "_posts/*.md", "_includes/*.html"],
+				tasks: ["shell:jekyllBuild"]
+			},
+
+			css: {
+				files: ['sass/*.scss'],
+				tasks: ["sass", "autoprefixer", "shell:jekyllBuild"],
+				options: {
+					spawn: false,
+				}
+			},
+			javascript: {
+				files: ['js/main.js'],
+				tasks: ['uglify']
+			}
+
+		}
+
+
+	});
+
+	// // Load the plugin that provides the "uglify" task.
+	// grunt.loadNpmTasks('grunt-contrib-uglify');
+	// grunt.loadNpmTasks('grunt-contrib-sass');
+	// grunt.loadNpmTasks('grunt-contrib-imagemin');
+	// grunt.loadNpmTasks('grunt-contrib-watch');
+	// grunt.loadNpmTasks('grunt-autoprefixer');
+
+	require("load-grunt-tasks")(grunt);
+
+
+	// Default task(s).
+	grunt.registerTask("serve", ["shell:jekyllServe"])
+	grunt.registerTask("default", ["sass","autoprefixer", "shell:jekyllBuild", "watch"]);
+
+};
---
+++ pricing-and-services.md
@@ -0,0 +1,5 @@
+---
+layout: pricing-and-services
+permalink: /pricing-and-services/
+title: Maple Maids - Pricing and Services
+---
